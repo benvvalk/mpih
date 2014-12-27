abe71c08f67e4dcb044a749d2f9e970e0bb3c475
Ben Vandervalk <benv@bcgsc.ca>
2014-12-27 14:23:02 -0500

    Command/init.h: initial work for 'SEND' cmd, code commenting

--- Command/init.h
+++ Command/init.h
@@ -50,21 +50,59 @@ static const struct option init_longopts[] = {
 
 enum ConnectionMode {
 	READING_COMMAND=0,
-	RECEIVING_DATA,
-	SENDING_DATA,
+	MPI_READY_TO_RECV_MSG_SIZE,
+	MPI_READY_TO_RECV_MSG,
+	MPI_READY_TO_SEND_MSG,
+	MPI_RECVING_MSG_SIZE,
+	MPI_RECVING_MSG,
+	MPI_SENDING_MSG,
 	CLOSED
 };
 
 #define MPI_BUFFER_SIZE 65536
 
 struct ConnectionState {
+
+	/** connection state (e.g. sending data) */
 	ConnectionMode mode;
-	size_t mpi_buffer_len;
+	/** remote rank for sending/receiving data */
+	int rank;
+	/** length of MPI send/recv buffer */
+	uint64_t mpi_buffer_len;
+	/** buffer for non-blocking MPI send/recv */
 	char* mpi_buffer;
+	/** ID for checking state of asynchronous send/recv */
+	MPI_Request size_msg_id;
+	/** ID for checking state of asynchronous send/recv */
+	MPI_Request body_msg_id;
+
+	ConnectionState() :
+		mode(READING_COMMAND),
+		rank(0),
+		mpi_buffer_len(0),
+		mpi_buffer(NULL),
+		size_msg_id(0),
+		body_msg_id(0)
+	{ }
+
+	void clear()
+	{
+		mode = READING_COMMAND;
+		rank = 0;
+		mpi_buffer_len = 0;
+		if (mpi_buffer != NULL)
+			free(mpi_buffer);
+		size_msg_id = 0;
+		body_msg_id = 0;
+	}
 };
+
+typedef std::map<evutil_socket_t, ConnectionState> ConnectionStateMap;
+ConnectionStateMap g_connectionStates;
 
 #define MAX_READ_SIZE 16384
 #define MAX_HEADER_SIZE 256
+#define MPI_DEFAULT_TAG 0
 
 static inline void
 close_connection(struct bufferevent* bev,
@@ -78,20 +116,77 @@ close_connection(struct bufferevent* bev,
 	bufferevent_free(bev);
 }
 
-static inline void
-process_header(const char* line, struct bufferevent *bev)
+static inline void post_mpi_send(struct bufferevent* bev,
+	ConnectionState* state)
 {
+	assert(bev != NULL);
+	assert(state != NULL);
+	assert(state->mode == MPI_READY_TO_SEND_MSG);
+
+	// send message size in advance of message body
+	MPI_Isend(&state->mpi_buffer_len, 1, MPI_UINT64_T,
+		state->rank, 0, MPI_COMM_WORLD,
+		&state->size_msg_id);
+
+	// send message body
+	assert(state->mpi_buffer != NULL);
+	MPI_Isend(state->mpi_buffer, state->mpi_buffer_len,
+		MPI_BYTE, state->rank, MPI_DEFAULT_TAG,
+		MPI_COMM_WORLD, &state->body_msg_id);
+
+	state->mode = MPI_SENDING_MSG;
+}
+
+static inline void
+process_header(const char* line, struct bufferevent *bev,
+	ConnectionState* state)
+{
+	assert(bev != NULL);
+	assert(state != NULL);
+
 	printf("Received command: '%s'\n", line);
 
-	if (strcmp(line, "RANK") == 0) {
+	std::stringstream ss(line);
+	std::string command;
+	ss >> command;
+
+	// empty or all-whitespace header line
+	if (command.empty())
+		return;
+
+	if (command == "RANK") {
+
 		assert(bev != NULL);
 		struct evbuffer* output = bufferevent_get_output(bev);
 		assert(output != NULL);
 		evbuffer_add_printf(output, "%d\n", mpi::rank);
+
+	} else if (command == "SEND") {
+
+		int rank;
+		size_t size;
+		ss >> rank;
+		ss >> size;
+
+		if (ss.fail() || !ss.eof()) {
+			fprintf(stderr, "malformed SEND header, "
+				"expected 'SEND <RANK> <BYTES>'\n");
+			return;
+		}
+
+		memset(state, 0, sizeof(ConnectionState));
+		state->mode = MPI_READY_TO_SEND_MSG;
+		state->rank = rank;
+		state->mpi_buffer_len = size;
+		state->mpi_buffer = (char *)malloc(size);
+
+		post_mpi_send(bev, state);
+
 	} else {
-		fprintf(stderr, "error: unrecognized command '%s'\n",
-			line);
+		fprintf(stderr, "error: unrecognized header '%s'\n",
+			command.c_str());
 	}
+
 }
 
 static inline void
@@ -109,7 +204,7 @@ init_read_handler(struct bufferevent *bev, void *arg)
 		size_t origLen = evbuffer_get_length(input);
 		char* line = evbuffer_readln(input, &n, EVBUFFER_EOL_LF);
 		if (line != NULL) {
-			process_header(line, bev);
+			process_header(line, bev, state);
 		} else if (origLen >= MAX_HEADER_SIZE) {
 			fprintf(stderr, "header line exceeded max length "
 				"(%d bytes)\n", MAX_HEADER_SIZE);
@@ -141,16 +236,17 @@ init_accept_handler(evutil_socket_t listener, short event, void *arg)
 {
 	struct event_base *base = (event_base*)arg;
 
+	// connect to client (or die)
 	evutil_socket_t fd = UnixSocket::accept(listener, false);
-
 	printf("Connected to client.\n");
 
+	// object to track state connection
 	ConnectionState* state = (ConnectionState *)
 		malloc(sizeof(ConnectionState));
+	memset(state, 0, sizeof(ConnectionState));
 	state->mode = READING_COMMAND;
-	state->mpi_buffer = NULL;
-	state->mpi_buffer_len = 0;
 
+	// create buffer and associate with new connection
 	struct bufferevent* bev = bufferevent_socket_new(base,
 		fd, BEV_OPT_CLOSE_ON_FREE);
 
@@ -163,13 +259,16 @@ init_accept_handler(evutil_socket_t listener, short event, void *arg)
 	bufferevent_enable(bev, EV_READ|EV_WRITE);
 }
 
-static inline void serverLoop(const char* socketPath)
+static inline void server_loop(const char* socketPath)
 {
+	// create Unix domain socket that listens for connections
 	evutil_socket_t listener = UnixSocket::listen(socketPath, false);
 
+	// main state object for libevent
 	struct event_base* base = event_base_new();
 	assert(base != NULL);
 
+	// register handler for new connections
 	struct event* listener_event = event_new(base, listener,
 		EV_READ|EV_PERSIST, init_accept_handler, (void*)base);
 
@@ -178,13 +277,17 @@ static inline void serverLoop(const char* socketPath)
 
 	printf("Waiting for connection...\n");
 
+	// start libevent loop
 	event_base_dispatch(base);
+
+	// cleanup
 	event_free(listener_event);
 	event_base_free(base);
 }
 
 static inline int cmd_init(int argc, char** argv)
 {
+	// parse command line options
 	for (int c; (c = getopt_long(argc, argv,
 		init_shortopts, init_longopts, NULL)) != -1;) {
 		std::istringstream arg(optarg != NULL ? optarg : "");
@@ -209,16 +312,19 @@ static inline int cmd_init(int argc, char** argv)
 	if (argc - optind != 1)
 		die(INIT_USAGE_MESSAGE);
 
-	// turn off buffering on stdout/stderr
-	setvbuf(stdout, NULL, _IONBF, 0);
-	setvbuf(stderr, NULL, _IONBF, 0);
+	// use line buffering on stdout/stderr
+	setvbuf(stdout, NULL, _IOLBF, 0);
+	setvbuf(stderr, NULL, _IOLBF, 0);
 
+	// initialize MPI
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi::numProc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi::rank);
 
-	serverLoop(argv[optind]);
+	// start connection handling loop on Unix socket
+	server_loop(argv[optind]);
 
+	// shutdown MPI
 	MPI_Finalize();
 
 	return 0;
