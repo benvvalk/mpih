530eb6f7675589ea0dc24b611abc6f625ccadf05
Ben Vandervalk <benv@bcgsc.ca>
2015-01-04 14:55:45 -0800

    IntegrationTest/hello-world.sh: cleanup and comments

--- Test/IntegrationTest/hello-world.sh
+++ Test/IntegrationTest/hello-world.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-set -o pipefail
+set -eu -o pipefail
 
-tmpdir=$(mktemp -d)
-
-# Client commands (e.g. 'mpih send') communicate
+# client commands (e.g. 'mpih send') communicate
 # with the 'mpih init' daemon using this Unix socket
+tmpdir=$(mktemp -d)
 export MPIH_SOCKET="$tmpdir/mpih_socket"
 
-# Start daemon that executes MPI routines
+# start daemon that executes MPI routines
 mpih init
-
-# Wait for daemon to start
+# wait for daemon to start up
 sleep 2
 
 my_rank=$(mpih rank)
 num_ranks=$(mpih size)
 
-dest_rank=$(( ($my_rank + 1) % $num_ranks ))
-
-if [ $my_rank -eq 0 ]; then
+dest_rank=$((($my_rank + 1) % $num_ranks))
+src_rank=$(($my_rank - 1))
+if [ $src_rank -lt 0 ]; then
 	src_rank=$(($num_ranks - 1))
-else
-	src_rank=$(($my_rank - 1))
 fi
 
-echo 'Hello, World!' | mpih send $dest_rank
+# send a message to a neighbour
+echo "Hello, from rank $my_rank!" | mpih send $dest_rank
 
+# receive a message from a neighbour
 msg=$(mpih recv $src_rank)
-
-echo "rank $my_rank received '$msg' from rank $src_rank"
+echo "rank $my_rank received: '$msg'"
 
 # shutdown daemon
 mpih finalize
