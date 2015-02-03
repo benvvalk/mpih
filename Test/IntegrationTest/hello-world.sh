a8301cb423fa5900f963b3c3a580606dd83d7316
Ben Vandervalk <benv@bcgsc.ca>
2015-02-02 21:27:59 -0800

    IntegrationTest/hello-world.sh: simplified version based on 'mpih run'

--- Test/IntegrationTest/hello-world.sh
+++ Test/IntegrationTest/hello-world.sh
@@ -1,32 +1,16 @@
 #!/bin/bash
-
 set -eu -o pipefail
 
-# client commands (e.g. 'mpih send') communicate
-# with the 'mpih init' daemon using this Unix socket
-tmpdir=$(mktemp -d)
-export MPIH_SOCKET="$tmpdir/mpih_socket"
-
-# start daemon that executes MPI routines
-mpih init
-# wait for daemon to start up
-sleep 2
-
-my_rank=$(mpih rank)
-num_ranks=$(mpih size)
-
-dest_rank=$((($my_rank + 1) % $num_ranks))
-src_rank=$(($my_rank - 1))
+# send 'hello' messages in a ring
+dest_rank=$((($MPIH_RANK + 1) % $MPIH_SIZE))
+src_rank=$(($MPIH_RANK - 1))
 if [ $src_rank -lt 0 ]; then
-	src_rank=$(($num_ranks - 1))
+	src_rank=$(($MPIH_SIZE - 1))
 fi
 
 # send a message to a neighbour
-echo "Hello, from rank $my_rank!" | mpih send $dest_rank
+echo "Hello, from rank $MPIH_RANK!" | mpih send $dest_rank
 
 # receive a message from a neighbour
 msg=$(mpih recv $src_rank)
-echo "rank $my_rank received: '$msg'"
-
-# shutdown daemon
-mpih finalize
+echo "rank $MPIH_RANK received: '$msg'"
