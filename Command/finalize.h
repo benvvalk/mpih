864369ec108a2408a0ecca32ec652f219bcbdd30
Ben Vandervalk <benv@bcgsc.ca>
2015-02-02 19:55:52 -0800

    Command/finalize.h: factor out finalize() method

--- Command/finalize.h
+++ Command/finalize.h
@@ -38,6 +38,32 @@ static const struct option finalize_longopts[] = {
 	{ NULL, 0, NULL, 0 }
 };
 
+static inline void finalize()
+{
+	int socket = UnixSocket::connect(opt::socketPath.c_str());
+
+	if (opt::verbose)
+		std::cerr << "Connected." << std::endl;
+
+	struct event_base* base = event_base_new();
+	assert(base != NULL);
+
+	struct bufferevent* bev = bufferevent_socket_new(base,
+		socket, BEV_OPT_CLOSE_ON_FREE);
+	assert(bev != NULL);
+
+	bufferevent_setcb(bev, NULL, NULL,
+		client_event_handler, NULL);
+	bufferevent_setwatermark(bev, EV_READ, 0, 0);
+	bufferevent_enable(bev, EV_READ|EV_WRITE);
+
+	// send command to 'mpi init' daemon
+	evbuffer_add_printf(bufferevent_get_output(bev), "FINALIZE\n");
+
+	event_base_dispatch(base);
+	event_base_free(base);
+}
+
 int cmd_finalize(int argc, char** argv)
 {
 	for (int c; (c = getopt_long(argc, argv,
@@ -64,28 +90,7 @@ int cmd_finalize(int argc, char** argv)
 		std::cerr << "Connecting to 'mpih init' process..."
 			<< std::endl;
 
-	int socket = UnixSocket::connect(opt::socketPath.c_str());
-
-	if (opt::verbose)
-		std::cerr << "Connected." << std::endl;
-
-	struct event_base* base = event_base_new();
-	assert(base != NULL);
-
-	struct bufferevent* bev = bufferevent_socket_new(base,
-		socket, BEV_OPT_CLOSE_ON_FREE);
-	assert(bev != NULL);
-
-	bufferevent_setcb(bev, NULL, NULL,
-		client_event_handler, NULL);
-	bufferevent_setwatermark(bev, EV_READ, 0, 0);
-	bufferevent_enable(bev, EV_READ|EV_WRITE);
-
-	// send command to 'mpi init' daemon
-	evbuffer_add_printf(bufferevent_get_output(bev), "FINALIZE\n");
-
-	event_base_dispatch(base);
-	event_base_free(base);
+	finalize();
 
 	return 0;
 }
