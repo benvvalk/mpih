53e883ae351479c76b21d4b53740ab15ef4b2e29
Ben Vandervalk <benv@bcgsc.ca>
2015-02-09 14:28:07 -0800

    overlapping-send-test.sh: test overlapping data streams

---
+++ Test/IntegrationTest/overlapping-send-test.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+set -eu
+
+#------------------------------------------------------------
+# helper functions
+#------------------------------------------------------------
+
+stderr() {
+	echo "$@" >&2
+}
+
+#------------------------------------------------------------
+# argument checking
+#------------------------------------------------------------
+
+if [ "$MPIH_SIZE" -ne 2 ]; then
+	if [ "$MPIH_RANK" -eq 0 ]; then
+		stderr "error: this MPI script must be run with exactly 2 processes"
+	fi
+	exit 1
+fi
+if [ $# -lt 1 ]; then
+	if [ "$MPIH_RANK" -eq 0 ]; then
+		stderr "Usage: $(basename $0) <num_lines>"
+		stderr "Example: $(basename $0) 100"
+	fi
+	exit 1
+fi
+
+n=$1; shift
+
+#------------------------------------------------------------
+# test
+#------------------------------------------------------------
+
+stderr "log for rank $MPIH_RANK: $MPIH_LOG"
+
+data_file=data.txt
+recv1_file=recv1.txt
+recv2_file=recv2.txt
+seq 1 $n > data.txt
+
+if [ $MPIH_RANK -eq 0 ]; then
+	mpih send 1 $data_file &
+	mpih send 1 $data_file &
+	wait
+else
+	expected_size=$(du -b $data_file | cut -f1)
+	expected_md5sum=$(md5sum $data_file | cut -d' ' -f1)
+
+	mpih recv 0 > $recv1_file &
+	mpih recv 0 > $recv2_file &
+	wait
+
+	recv1_size=$(du -b $recv1_file | cut -f1)
+	recv1_md5sum=$(md5sum $recv1_file | cut -d' ' -f1)
+	recv2_size=$(du -b $recv2_file | cut -f1)
+	recv2_md5sum=$(md5sum $recv2_file | cut -d' ' -f1)
+
+	if [ "$recv1_size" -ne "$expected_size" ] || \
+		[ "$recv1_md5sum" != "$expected_md5sum" ] || \
+		[ "$recv2_size" -ne "$expected_size" ] || \
+		[ "$recv2_md5sum" != "$expected_md5sum" ]; then
+		stderr "FAILED!:"
+		stderr "  test data: $data_file"
+		stderr "  test data size (bytes): $expected_size"
+		stderr "  test data md5sum: $expected_size"
+		stderr "  recv #1 data: $recv1_file"
+		stderr "  recv #1 size (bytes): $recv1_size"
+		stderr "  recv #1 md5sum: $recv1_md5sum"
+		stderr "  recv #2 data: $recv1_file"
+		stderr "  recv #2 size (bytes): $recv2_size"
+		stderr "  recv #2 md5sum: $recv2_md5sum"
+		exit 1
+	else
+		stderr "PASSED!"
+	fi
+fi
