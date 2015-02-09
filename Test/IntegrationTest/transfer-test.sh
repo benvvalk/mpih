4bc1898733c01ad67a43e85ba60064000718699c
Ben Vandervalk <benv@bcgsc.ca>
2015-02-08 16:28:28 -0800

    IntegrationTest/transfer-test.sh: clean up / simplification

--- Test/IntegrationTest/transfer-test.sh
+++ Test/IntegrationTest/transfer-test.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 set -eu -o pipefail
 
-if [ $# -ne 1 ]; then
-	echo "Usage: mpirun -np <num_ranks> $(basename $0) <file_size>" >&2
-	echo "Example: mpirun -np 2 $(basename $0) 1M" >&2
+#------------------------------------------------------------
+# helper functions
+#------------------------------------------------------------
+
+stderr() {
+	echo "$@" >&2
+}
+
+#------------------------------------------------------------
+# argument checking
+#------------------------------------------------------------
+
+if [ "$MPIH_SIZE" -ne 2 ]; then
+	if [ "$MPIH_RANK" -eq 0 ]; then
+		stderr "error: this MPI script must be run with exactly 2 processes"
+	fi
+	exit 1
+fi
+if [ $# -lt 1 ]; then
+	if [ "$MPIH_RANK" -eq 0 ]; then
+		stderr "Usage: $(basename $0) <size>"
+		stderr "Example: $(basename $0) 1M"
+	fi
 	exit 1
 fi
 
-file_size=$1; shift
+size=$1; shift
 
-# client commands (e.g. 'mpih send') communicate
-# with the 'mpih init' daemon using this Unix socket
-tmpdir=$(mktemp -d)
-export MPIH_SOCKET="$tmpdir/mpih_socket"
-
-# start daemon that executes MPI routines
-mpih init -vvv --log $tmpdir/init.log
-# wait for daemon to start up
-sleep 2
-
-my_rank=$(mpih rank)
-num_ranks=$(mpih size)
-max_rank=$(($num_ranks - 1))
+#------------------------------------------------------------
+# run test
+#------------------------------------------------------------
 
 data_file=random.bin
-
-if [ $my_rank -eq 0 ]; then
-	head -c $file_size /dev/urandom > $data_file
-	for i in $(seq 0 $max_rank); do
-		mpih send $i random.bin &
-	done
-fi
-
-my_md5sum=$(mpih recv 0 | md5sum | cut -d' ' -f1)
-correct_md5sum=$(md5sum $data_file | cut -d' ' -f1)
-
-if [ "$my_md5sum" == "$correct_md5sum" ]; then
-	echo "success: data recv'ed by rank $my_rank is correct!"
+if [ $MPIH_RANK -eq 0 ]; then
+	dd if=/dev/urandom count=1 bs=$size > $data_file 2>/dev/null
+	mpih send 1 random.bin
 else
-	echo "error: data recv'ed by rank $my_rank differs from data sent!"
-fi
+	my_md5sum=$(mpih recv 0 | md5sum | cut -d' ' -f1)
+	correct_md5sum=$(md5sum $data_file | cut -d' ' -f1)
 
-# shutdown daemon
-mpih finalize
+	if [ "$my_md5sum" == "$correct_md5sum" ]; then
+		stderr "PASSED: received data identical to sent data!"
+	else
+		stderr "FAILED: received data differs from sent data!"
+	fi
+fi
