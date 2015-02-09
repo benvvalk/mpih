70701a31b1416642135ae93dee4c1c9b744540a1
Ben Vandervalk <benv@bcgsc.ca>
2015-02-09 13:43:23 -0800

    tandem-send-test.sh: use human-readable test data (for easier debugging)

--- Test/IntegrationTest/tandem-send-test.sh
+++ Test/IntegrationTest/tandem-send-test.sh
@@ -21,20 +21,24 @@ if [ "$MPIH_SIZE" -ne 2 ]; then
 fi
 if [ $# -lt 1 ]; then
 	if [ "$MPIH_RANK" -eq 0 ]; then
-		stderr "Usage: $(basename $0) <size>"
-		stderr "Example: $(basename $0) 1M"
+		stderr "Usage: $(basename $0) <num_lines>"
+		stderr "Example: $(basename $0) 100"
 	fi
 	exit 1
 fi
 
-size=$1; shift
+n=$1; shift
 
 #------------------------------------------------------------
 # test
 #------------------------------------------------------------
 
-data_file=random.bin
-dd if=/dev/urandom count=1 bs=$size >$data_file 2>/dev/null
+stderr "log for rank $MPIH_RANK: $MPIH_LOG"
+
+data_file=data.txt
+recv1_file=recv1.txt
+recv2_file=recv2.txt
+seq 1 $n > data.txt
 
 if [ $MPIH_RANK -eq 0 ]; then
 	mpih send 1 $data_file
@@ -42,19 +46,19 @@ if [ $MPIH_RANK -eq 0 ]; then
 else
 	expected_size=$(du -b $data_file | cut -f1)
 	expected_md5sum=$(md5sum $data_file | cut -d' ' -f1)
-	mpih recv 0 > recv1.bin
-	recv1_size=$(du -b recv1.bin | cut -f1)
-	recv1_md5sum=$(md5sum recv1.bin | cut -d' ' -f1)
-	mpih recv 0 > recv2.bin
-	recv2_size=$(du -b recv2.bin | cut -f1)
-	recv2_md5sum=$(md5sum recv2.bin | cut -d' ' -f1)
+	mpih recv 0 > $recv1_file
+	recv1_size=$(du -b $recv1_file | cut -f1)
+	recv1_md5sum=$(md5sum $recv1_file | cut -d' ' -f1)
+	mpih recv 0 > $recv2_file
+	recv2_size=$(du -b $recv2_file | cut -f1)
+	recv2_md5sum=$(md5sum $recv2_file | cut -d' ' -f1)
 	if [ "$recv1_size" -ne "$expected_size" ]; then
 		echo "FAILED: data size for recv #1 does not match expected size"
 		stderr "correct size (bytes): $expected_size"
 		stderr "recv #1 size (bytes): $recv1_size"
 		exit 1
 	elif [ "$recv1_md5sum" != "$expected_md5sum" ]; then
-		stderr "FAILED: md5sum for recv #1 does not match expected size"
+		stderr "FAILED: md5sum for recv #1 does not match md5sum"
 		stderr "correct md5sum: $expected_md5sum"
 		stderr "recv #1 md5sum: $recv1_md5sum"
 		exit 1
